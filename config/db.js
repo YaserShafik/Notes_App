570b26fd2d0d350bdedb35c14af70629d8f50653
Yaser Shafik <76571382+YaserShafik@users.noreply.github.com>
2024-07-11 17:27:30 +0200

    Update db.js
    Removed db connection string

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbUrl = "mongodb+srv://admin:pies3,14@cluster0.2ysbtpk.mongodb.net/Notes_DB?retryWrites=true&w=majority&appName=Cluster0";
-
+const dbUrl;
 const connectDB = async () => {
     try {
         await mongoose.connect(dbUrl, {
